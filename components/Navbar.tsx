d158e0d5e6fae1d67fa0d8cef7d592adbb06f852
Auriel James <aurieljames11@gmail.com>
2024-09-19 01:14:19 +0800

    ViewItem UI Done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { FiAlignRight } from "react-icons/fi";
 import { IoPersonOutline } from "react-icons/io5";
 import { LuHeart } from "react-icons/lu";
-import { FiShoppingBag } from "react-icons/fi";
+import { BsCart } from "react-icons/bs";
 import { IoIosSearch } from "react-icons/io";
 import { Button, Popover } from "flowbite-react";
 import { FiSettings } from "react-icons/fi";
@@ -22,9 +22,9 @@ const Navbar = async() => {
 
     return <nav className={`backdrop-blur-sm bg-opacity-80 shadow border-black bg-white w-full flex items-center sticky top-0 z-50 ` + (session?.user ? '' : 'hidden ')}>
         <div className="flex w-full items-center justify-between my-4 mx-6 md:mx-12">
-            <Link className="logo font-black text-blue-800 text-2xl flex items-center gap-2" href="/">
-                <Image width={30} height={30} alt="logo" src={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAMAAADW3miqAAAAVFBMVEUMIU4WL2cfP4IsUqUuVqsePH4WMWkMIE0HGkAJGUMAAAAnS5gvVKcjRYwuVqsrUKEnS5glR5EiQ4ogQIQePH4bOHYYM28VL2cTK18QJlcNIk8JHEXcW5ykAAAADnRSTlPf39/t36Cfn10fAC9ZX0CfO88AAADVSURBVHjatZTNDoJADIRnNh5ITPTi+7+dR+NBvRh1BArlpymSELtL+em3pR3I8gCARDto3m4uBb2VhqkPP9HdYIYL8xAjxLA4ZJonYAIllkJMISZxh+hcZjtAtPjVpOh9NYEIY4anm7rjGgiboV0aURlBTD7w6yihmXgsv05DTfxduJbTqIWYyqRUgshpgP6ruHoxP33R++e0qXftHfJlJ0A2zI3FDCZ3sGURUriaQVrXnRDpR1UWOlfHfFKd1KH3SihpHjrjUCjK5q3S9B8/x12htAy+i+5EJWkG0ucAAAAASUVORK5CYII='} />
-                <p className="">CSLVEO</p>
+            <Link className="logo font-black text-yellow-700 text-2xl flex items-center gap-1" href="/">
+                {/* <Image width={28} height={28} alt="logo" src={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAMAAADW3miqAAAAVFBMVEUMIU4WL2cfP4IsUqUuVqsePH4WMWkMIE0HGkAJGUMAAAAnS5gvVKcjRYwuVqsrUKEnS5glR5EiQ4ogQIQePH4bOHYYM28VL2cTK18QJlcNIk8JHEXcW5ykAAAADnRSTlPf39/t36Cfn10fAC9ZX0CfO88AAADVSURBVHjatZTNDoJADIRnNh5ITPTi+7+dR+NBvRh1BArlpymSELtL+em3pR3I8gCARDto3m4uBb2VhqkPP9HdYIYL8xAjxLA4ZJonYAIllkJMISZxh+hcZjtAtPjVpOh9NYEIY4anm7rjGgiboV0aURlBTD7w6yihmXgsv05DTfxduJbTqIWYyqRUgshpgP6ruHoxP33R++e0qXftHfJlJ0A2zI3FDCZ3sGURUriaQVrXnRDpR1UWOlfHfFKd1KH3SihpHjrjUCjK5q3S9B8/x12htAy+i+5EJWkG0ucAAAAASUVORK5CYII='} /> */}
+                <p className="">CHAESTHETICS</p>
             </Link>
             <div className="flex items-center gap-20 w-full  justify-end lg:justify-between">
                 <div className="w-full hidden lg:flex items-center text-[11px] font-medium gap-10 justify-end">
@@ -47,7 +47,7 @@ const Navbar = async() => {
                 <div className="flex items-center gap-4 md:gap-6">
                     <SearchDrawer />
                     <div className="hover:cursor-pointer">
-                        <FiShoppingBag size={20}/>
+                        <BsCart size={20}/>
                     </div>
                     <div className="hover:cursor-pointer">
                         <LuHeart size={20}/>
